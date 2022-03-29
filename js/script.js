9acdb06850cb18e6ae7140c29a29e7f355b9485e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-29 18:12:48 +0300

    feat: add scroll animation

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ new TypeIt("#printAnimation", {
 }).go();
 
 // Burger
-
 const iconMenu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
 
@@ -28,3 +27,42 @@ menuLinks.forEach((item) => {
     }
   });
 });
+
+// Scroll animation
+
+const animItems = document.querySelectorAll("._anim-item");
+console.log(animItems);
+if (animItems.length > 0) {
+  animOnScroll();
+  window.addEventListener("scroll", animOnScroll);
+}
+
+function animOnScroll() {
+  for (let i = 0; i < animItems.length; i++) {
+    const animItem = animItems[i];
+    const animItemHeight = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = 4;
+
+    let animItemPoint = window.innerHeight - animItemHeight / animStart;
+    if (animItemHeight > window.innerHeight) {
+      animItemPoint = window.innerHeight - window.innerHeight / animStart;
+    }
+
+    if (
+      pageYOffset > animItemOffset - animItemPoint &&
+      pageYOffset < animItemOffset + animItemHeight
+    ) {
+      animItem.classList.add("_active");
+    } else {
+    //   animItem.classList.remove("_active");
+    }
+  }
+}
+
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
