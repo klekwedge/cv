fbb8cea9411838f1eb66106f1ec6ab7dacc630a6
klekkwedge <antonov.andrey.10101@gmail.com>
2022-03-29 10:06:47 +0300

    fix: add burger menu

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// TypeIt
+new TypeIt("#printAnimation", {
+  strings: "Hi, I'm Andrey 👋",
+}).go();
+
+// Burger
+
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", (event) => {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
+
+const menuLinks = document.querySelectorAll(".menu__link");
+menuLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (iconMenu.classList.contains("_active")) {
+      document.body.classList.remove("_lock");
+      iconMenu.classList.remove("_active");
+      menuBody.classList.remove("_active");
+    }
+  });
+});
