b5dbe030e399fa617eb78bf72f6e9d735e03e731
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-03 13:19:07 +0300

    feat: add check for support of webp format

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 "use strict";
+import isWebp from './modules/webp.js'
 import burgerMenu from './modules/burger-menu.js'
 import scrollAnimation from './modules/scroll-animation.js'
 import typeIt from './modules/typeIt.js'
 
+
 window.addEventListener("DOMContentLoaded", () => {
+  isWebp();
   typeIt();
   burgerMenu();
   scrollAnimation();
