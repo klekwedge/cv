81d2c23f6f93a0940b0892981ed309cbb4003f68
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-06 15:22:00 +0300

    refactor: change structure of the project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+import burgerMenu from './modules/burger-menu.js'
+import scrollAnimation from './modules/scroll-animation.js'
+import typeIt from './modules/typeIt.js'
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  typeIt();
+  burgerMenu();
+  scrollAnimation();
+});
