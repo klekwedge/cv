87ee0a0ad7b47aba8d5862464f88eb073b8df21f
Andrey <antonov.andrey.10101@gmail.com>
2022-11-22 21:10:03 +0300

    feat: add burger menu functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,25 @@
+import { useRef } from "react";
 
 function Header() {
+  const iconMenu = useRef<HTMLDivElement>(null);
+  const menuBody = useRef<HTMLElement>(null);
+  const menuLinks = useRef();
+
+  const iconMenuToggle = () => {
+    if (iconMenu.current && menuBody.current) {
+      document.body.classList.toggle("_lock");
+      iconMenu.current.classList.toggle("_active");
+      menuBody.current.classList.toggle("_active");
+    }
+  };
+
+  const menuLinksToggle = () => {
+    if (iconMenu.current && iconMenu.current.classList.contains("_active")) {
+      document.body.classList.remove("_lock");
+      iconMenu.current.classList.remove("_active");
+      menuBody.current?.classList.remove("_active");
+    }
+  };
 
   return (
     <header className="header">
@@ -9,29 +29,53 @@ function Header() {
             <span>A</span>
             Andrey Antonov
           </h1>
-          <nav className="menu__body">
+          <nav className="menu__body" ref={menuBody}>
             <ul className="menu__list">
               <li className="menu__item">
-                <a href="#about" className="menu__link _link">Обо мне </a>
+                <a
+                  href="#about"
+                  className="menu__link _link"
+                  onClick={menuLinksToggle}
+                >
+                  Обо мне{" "}
+                </a>
               </li>
               <li className="menu__item">
-                <a href="#skills" className="menu__link _link">Навыки</a>
+                <a
+                  href="#skills"
+                  className="menu__link _link"
+                  onClick={menuLinksToggle}
+                >
+                  Навыки
+                </a>
               </li>
               <li className="menu__item">
-                <a href="#strengths" className="menu__link _link">Сильные стороны</a>
+                <a
+                  href="#strengths"
+                  className="menu__link _link"
+                  onClick={menuLinksToggle}
+                >
+                  Сильные стороны
+                </a>
               </li>
               <li className="menu__item">
-                <a href="#projects" className="menu__link _link">Проекты</a>
+                <a
+                  href="#projects"
+                  className="menu__link _link"
+                  onClick={menuLinksToggle}
+                >
+                  Проекты
+                </a>
               </li>
             </ul>
           </nav>
-          <div className="menu__icon">
+          <div className="menu__icon" ref={iconMenu} onClick={iconMenuToggle}>
             <span></span>
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header;
