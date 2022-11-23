b7501836977938df77da5a3d8f4d8682c3eba65c
Andrey <antonov.andrey.10101@gmail.com>
2022-11-23 13:31:53 +0300

    feat: change the photo on the welcome page

--- src/components/Greetings/Greetings.tsx
+++ src/components/Greetings/Greetings.tsx
@@ -1,30 +1,31 @@
 import { EMAIL_ID, RESUME_LINK } from "../../root.link";
+import './Greetings.scss';
 
 function Greetings() {
   return (
     <section className="main__greetings greetings">
       <div className="greetings__container _container">
-        <h2 className="greetings__name">Привет, меня зовут Антонов Андрей</h2>
-        <h3 className="greetings__qualification _anim-item text-focus-in">
-          Front-End разработчик
-        </h3>
-        <div className="greetings__buttons">
-          <a className="greetings__button" href={`mailto:${EMAIL_ID}`}>
-            <img src="./assets/svg/mail-icon.svg" /> Напишите мне
-          </a>
-          <a
-            className="greetings__button"
-            download
-            href={RESUME_LINK}
-            title="Download CV in PDF format"
-          >
-            <img src="./assets/svg/download-icon.svg" />
-            Скачать резюме
-          </a>
+        <div className="greetings__info">
+          <h2 className="greetings__name">Привет, меня зовут Андрей</h2>
+          <h3 className="greetings__qualification _anim-item text-focus-in">
+            Я Front-End разработчик
+          </h3>
+          <div className="greetings__buttons">
+            <a className="greetings__button" href={`mailto:${EMAIL_ID}`}>
+              <img src="./assets/svg/mail-icon.svg" /> Напишите мне
+            </a>
+            <a
+              className="greetings__button"
+              download
+              href={RESUME_LINK}
+              title="Download CV in PDF format"
+            >
+              <img src="./assets/svg/download-icon.svg" />
+              Скачать резюме
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="greetings__image _ibg">
-        <img src="assets/img/bg.jpg" alt="Background photo" />
+        <img className="greetings__image" src="/public/assets/svg/illustration.svg" />
       </div>
     </section>
   );
