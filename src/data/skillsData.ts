378d971aa0daf0cd885a47ed03cd68d89f5e7799
Andrey <antonov.andrey.10101@gmail.com>
2022-11-23 17:29:37 +0300

    feat: change technologies used in projects

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,128 +1,127 @@
-
-
 const Proficient = [
   {
     skill: "HTML",
-    logo: '/public/assets/svg/skills/html.svg'
+    logo: "/public/assets/svg/skills/html.svg",
   },
   {
     skill: "CSS",
-    logo: '/public/assets/svg/skills/css.svg'
+    logo: "/public/assets/svg/skills/css.svg",
   },
   {
     skill: "SASS / SCSS",
-    logo: '/public/assets/svg/skills/sass.svg'
+    logo: "/public/assets/svg/skills/sass.svg",
   },
   {
     skill: "Tailwind CSS",
-    logo: '/public/assets/svg/skills/tailwindcss.svg'
+    logo: "/public/assets/svg/skills/tailwindcss.svg",
   },
   {
     skill: "Javascript",
-    logo: '/public/assets/svg/skills/js.svg'
+    logo: "/public/assets/svg/skills/js.svg",
   },
   {
     skill: "TypeScript",
-    logo: '/public/assets/svg/skills/ts.svg'
+    logo: "/public/assets/svg/skills/ts.svg",
   },
-  {
-    skill: "Python",
-    logo: '/public/assets/svg/skills/python.svg'
-  },
+
   {
     skill: "Git",
-    logo: '/public/assets/svg/skills/git.svg'
+    logo: "/public/assets/svg/skills/git.svg",
   },
   {
     skill: "React",
-    logo: '/public/assets/svg/skills/react.svg'
+    logo: "/public/assets/svg/skills/react.svg",
   },
   {
     skill: "React Router",
-    logo: '/public/assets/svg/skills/react-router.svg'
+    logo: "/public/assets/svg/skills/react-router.svg",
   },
   {
     skill: "Redux",
-    logo: '/public/assets/svg/skills/redux.svg'
+    logo: "/public/assets/svg/skills/redux.svg",
   },
   {
     skill: "Redux Toolkit",
-    logo: '/public/assets/svg/skills/redux.svg'
+    logo: "/public/assets/svg/skills/redux.svg",
   },
   {
     skill: "Chakra UI",
-    logo: '/public/assets/svg/skills/chakra.svg'
+    logo: "/public/assets/svg/skills/chakra.svg",
   },
   {
     skill: "Vue",
-    logo: '/public/assets/svg/skills/vue.svg'
+    logo: "/public/assets/svg/skills/vue.svg",
   },
 
   {
     skill: "Vuex",
-    logo: '/public/assets/svg/skills/vue.svg'
+    logo: "/public/assets/svg/skills/vue.svg",
   },
   {
     skill: "Vue Router",
-    logo: '/public/assets/svg/skills/vue.svg'
+    logo: "/public/assets/svg/skills/vue.svg",
   },
   {
     skill: "Vuetify",
-    logo: '/public/assets/svg/skills/vuetifyjs.svg'
+    logo: "/public/assets/svg/skills/vuetifyjs.svg",
   },
   {
     skill: "Figma",
-    logo: '/public/assets/svg/skills/figma.svg'
+    logo: "/public/assets/svg/skills/figma.svg",
   },
   {
     skill: "GitHub",
-    logo: '/public/assets/svg/skills/github.svg'
+    logo: "/public/assets/svg/skills/github.svg",
   },
   {
     skill: "GitLab",
-    logo: '/public/assets/svg/skills/gitlab.svg'
+    logo: "/public/assets/svg/skills/gitlab.svg",
   },
 ];
 const Familiar = [
   {
     skill: "Webpack",
-    logo: '/public/assets/svg/skills/webpack.svg'
+    logo: "/public/assets/svg/skills/webpack.svg",
   },
   {
     skill: "Gulp",
-    logo: '/public/assets/svg/skills/gulp.svg'
+    logo: "/public/assets/svg/skills/gulp.svg",
   },
   {
     skill: "Next",
-    logo: '/public/assets/svg/skills/next.svg'
+    logo: "/public/assets/svg/skills/next.svg",
   },
   {
     skill: "Node",
-    logo: '/public/assets/svg/skills/node.svg'
+    logo: "/public/assets/svg/skills/node.svg",
   },
   {
     skill: "jQuery",
-    logo: '/public/assets/svg/skills/jquery.svg'
+    logo: "/public/assets/svg/skills/jquery.svg",
   },
   {
     skill: "Pug",
-    logo: '/public/assets/svg/skills/pug.svg'
+    logo: "/public/assets/svg/skills/pug.svg",
   },
   {
     skill: "Chart.js",
-    logo: '/public/assets/svg/skills/chart.svg'
+    logo: "/public/assets/svg/skills/chart.svg",
   },
   {
     skill: "Redux Saga",
-    logo: '/public/assets/svg/skills/redux-saga.svg'
+    logo: "/public/assets/svg/skills/redux-saga.svg",
   },
   {
     skill: "C++",
-    logo: '/public/assets/svg/skills/cplusplus.svg'
+    logo: "/public/assets/svg/skills/cplusplus.svg",
+  },
+  {
+    skill: "Python",
+    logo: "/public/assets/svg/skills/python.svg",
   },
   {
     skill: "Java",
-    logo: '/public/assets/svg/skills/java.svg'
+    logo: "/public/assets/svg/skills/java.svg",
   },
 ];
 export { Proficient, Familiar };
