f51cd2cc0d1488c249a757625056e40d605cefa9
Andrey <antonov.andrey.10101@gmail.com>
2022-11-23 09:49:36 +0300

    feat: add scroll animation functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,70 @@
-import About from '../About/About'
-import Footer from '../Footer/Footer'
-import Greetings from '../Greetings/Greetings'
-import Header from '../Header/Header'
-import Projects from '../Projects/Projects'
-import Skills from '../Skills/Skills'
-import Strengths from '../Strengths/Strengths'
-import './App.css'
+import { useEffect } from "react";
+import About from "../About/About";
+import Footer from "../Footer/Footer";
+import Greetings from "../Greetings/Greetings";
+import Header from "../Header/Header";
+import Projects from "../Projects/Projects";
+import Skills from "../Skills/Skills";
+import Strengths from "../Strengths/Strengths";
+import "./App.css";
 
 function App() {
 
+
+  function offset(el: any) {
+    const rect = el.getBoundingClientRect();
+    const scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+
+  function animOnScroll(animItems: any) {
+    for (let i = 0; i < animItems.length; i += 1) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        window.scrollY > animItemOffset - animItemPoint &&
+        window.scrollY < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      }
+    }
+  }
+
+
+  useEffect(() => {
+    const animItems = document.querySelectorAll("._anim-item");
+    window.addEventListener("scroll", () => animOnScroll(animItems));
+
+    return () => {
+      window.removeEventListener("scroll", () => animOnScroll(animItems));
+    };
+  }, []);
+
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <main className="main">
-        <Greetings/>
+        <Greetings />
         <div className="main__container _container">
-          <About/>
-          <Skills/>
-          <Strengths/>
-          <Projects/>
+          <About />
+          <Skills />
+          <Strengths />
+          <Projects />
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
