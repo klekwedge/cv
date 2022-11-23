7fc8460c71791ee5f101cad22b0bfae0f50816e6
Andrey <antonov.andrey.10101@gmail.com>
2022-11-23 11:10:33 +0300

    feat: change contacts in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,48 +1,58 @@
+import {
+  EMAIL_ID,
+  GITHUB_USERNAME,
+  LINKEDIN_ID,
+  TELEGRAM_ID,
+} from "../../root.link";
 
 function Footer() {
-
   return (
     <footer className="footer">
-    <div className="footer__container _container">
-      <ul className="footer__list">
-        <li className="footer__item">
-          <ul className="footer__list-contacts contacts">
-            <li className="contacts__item">
-              Телефон:
-              <a className="_link" href="tel:+79112780000">+7 911 278 ** **</a>
-            </li>
-            <li className="contacts__item">
-              Почта:
-              <a className="_link" href="mailto:antonov.andrey.10101@gmail.com">antonov.andrey.10101@gmail.com</a>
-            </li>
-          </ul>
-        </li>
-        <li className="footer__item">
-          <ul className="footer__list-links links">
-            <li className="links__item">
-              <a href="https://t.me/klekkwedge">
-                <img className="icon__image _icon" src="assets/svg/telegram.svg" alt="Telegram icon" />
-              </a>
-            </li>
-            <li className="links__item">
-              <a href="https://www.linkedin.com/in/klekwedge/">
-                <img className="icon__image _icon" src="assets/svg/linkedIn.svg" alt="Linkedin logo" />
-              </a>
-            </li>
-            <li className="links__item">
-              <a href="https://github.com/klekwedge">
-                <img className="icon__image _icon" src="assets/svg/github.svg" alt="Github logo" />
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <div className="footer__copyright">
-        <div>© 2022 by Andrey Antonov</div>
+      <div className="footer__container _container">
+        <ul className="footer__list">
+          <li className="footer__item">
+            <ul className="footer__list-links links">
+              <li className="links__item">
+                <a href={`https://t.me/${TELEGRAM_ID}/`}>
+                  <img
+                    className="icon__image _icon"
+                    src="assets/svg/telegram.svg"
+                    alt="Telegram icon"
+                  />
+                </a>
+              </li>
+              <li className="links__item">
+                <a href={`https://www.linkedin.com/in/${LINKEDIN_ID}/`}>
+                  <img
+                    className="icon__image _icon"
+                    src="assets/svg/linkedIn.svg"
+                    alt="Linkedin logo"
+                  />
+                </a>
+              </li>
+              <li className="links__item">
+                <a href={`https://github.com/${GITHUB_USERNAME}/`}>
+                  <img
+                    className="icon__image _icon"
+                    src="assets/svg/github.svg"
+                    alt="Github logo"
+                  />
+                </a>
+              </li>
+              <li className="links__item">
+                <a className="_link" href={`mailto:${EMAIL_ID}`}>
+                  Почта
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <div className="footer__copyright">
+          <div>© 2022 by Andrey Antonov</div>
+        </div>
       </div>
-    </div>
-  </footer>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/root.link.ts
+++ src/root.link.ts
@@ -1,1 +1,4 @@
 export const GITHUB_USERNAME = 'klekwedge';
+export const EMAIL_ID = 'antonov.andrey.10101@gmail.com';
+export const LINKEDIN_ID = 'klekwedge';
+export const TELEGRAM_ID = 'klekkwedge';
