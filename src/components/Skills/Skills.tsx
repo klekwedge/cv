c5ba968e17968fa3ab7fcad1a2fd8a62e467e00f
Andrey <antonov.andrey.10101@gmail.com>
2022-11-24 11:07:38 +0300

    feat: add adaptive for skills section

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,25 +3,23 @@ import "./Skills.scss";
 
 function Skills() {
   return (
-    <section className="my-28  px-5" id="skills">
+    <section className="skills" id="skills">
       <h2 className="_title">Навыки и инструменты</h2>
       <div className="_heading-decoration"></div>
 
       {Proficient?.length > 0 && (
-        <div className="pl-3">
-          <h2 className="text-2xl"> Опытный </h2>
-          <div className="my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center">
+        <div className="skills__experienced experienced">
+          <h2 className="experienced__title"> Опытный </h2>
+          <div className="experienced__list">
             {Proficient.map((x, index) => (
               <div
-                className="flex flex-col items-center"
+                className="experienced__item"
                 tabIndex={0}
                 role="img"
                 aria-label={x.skill}
                 key={index}
               >
-                <div className="mb-3">
-                  <img src={x.logo} alt={x.logo} width="60" height="60" />
-                </div>
+                <img src={x.logo} alt={x.logo} width="60" height="60" />
                 {x.skill}
               </div>
             ))}
@@ -29,20 +27,18 @@ function Skills() {
         </div>
       )}
       {Familiar?.length > 0 && (
-        <div className="pl-3">
-          <h2 className="text-2xl"> Базовый </h2>
-          <div className="my-7 grid gap-5 grid-cols-3 md:grid-cols-6 content-center">
+        <div className="skills__basic basic">
+          <h2 className="basic__title"> Базовый </h2>
+          <div className="basic__list">
             {Familiar.map((x, index) => (
               <div
-                className="flex flex-col items-center"
+                className="basic__item"
                 tabIndex={0}
                 role="img"
                 aria-label={x.skill}
                 key={index}
               >
-                <div className="mb-3">
-                  <img src={x.logo} alt={x.logo} width="60" height="60" />
-                </div>
+                <img src={x.logo} alt={x.logo} width="60" height="60" />
 
                 {x.skill}
               </div>
