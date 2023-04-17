12750fe629e9ac3f0370d192fdb556bca00f7677
Andrey <antonov.andrey.10101@gmail.com>
2023-04-17 18:23:40 +0300

    feat: update skills

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,119 +1,139 @@
 const Proficient = [
   {
     skill: "HTML",
-    logo: "assets/svg/skills/html.svg",
+    logo: "assets/svg/skills/proficient/html.svg",
   },
   {
     skill: "CSS",
-    logo: "assets/svg/skills/css.svg",
+    logo: "assets/svg/skills/proficient/css.svg",
   },
   {
     skill: "SASS / SCSS",
-    logo: "assets/svg/skills/sass.svg",
+    logo: "assets/svg/skills/proficient/sass.svg",
   },
   {
     skill: "Tailwind CSS",
-    logo: "assets/svg/skills/tailwindcss.svg",
+    logo: "assets/svg/skills/proficient/tailwindcss.svg",
   },
   {
     skill: "Javascript",
-    logo: "assets/svg/skills/js.svg",
+    logo: "assets/svg/skills/proficient/js.svg",
   },
   {
     skill: "TypeScript",
-    logo: "assets/svg/skills/ts.svg",
+    logo: "assets/svg/skills/proficient/ts.svg",
   },
 
   {
     skill: "Git",
-    logo: "assets/svg/skills/git.svg",
+    logo: "assets/svg/skills/proficient/git.svg",
   },
   {
     skill: "React",
-    logo: "assets/svg/skills/react.svg",
+    logo: "assets/svg/skills/proficient/react.svg",
   },
   {
     skill: "React Router",
-    logo: "assets/svg/skills/react-router.svg",
+    logo: "assets/svg/skills/proficient/react-router.svg",
   },
   {
-    skill: "Redux",
-    logo: "assets/svg/skills/redux.svg",
-  },
-  {
-    skill: "Redux Toolkit",
-    logo: "assets/svg/skills/redux.svg",
+    skill: "Redux / Redux Toolkit",
+    logo: "assets/svg/skills/proficient/redux.svg",
   },
   {
     skill: "Chakra UI",
-    logo: "assets/svg/skills/chakra.svg",
+    logo: "assets/svg/skills/proficient/chakra.svg",
   },
   {
     skill: "Vue",
-    logo: "assets/svg/skills/vue.svg",
+    logo: "assets/svg/skills/proficient/vue.svg",
   },
-
   {
     skill: "Vuex",
-    logo: "assets/svg/skills/vue.svg",
+    logo: "assets/svg/skills/proficient/vue.svg",
   },
   {
     skill: "Vue Router",
-    logo: "assets/svg/skills/vue.svg",
+    logo: "assets/svg/skills/proficient/vue.svg",
   },
   {
     skill: "Vuetify",
-    logo: "assets/svg/skills/vuetifyjs.svg",
-  },
-  {
-    skill: "Figma",
-    logo: "assets/svg/skills/figma.svg",
+    logo: "assets/svg/skills/proficient/vuetifyjs.svg",
   },
 ];
+
 const Familiar = [
   {
     skill: "Webpack",
-    logo: "assets/svg/skills/webpack.svg",
+    logo: "assets/svg/skills/basic/webpack.svg",
   },
   {
     skill: "Gulp",
-    logo: "assets/svg/skills/gulp.svg",
+    logo: "assets/svg/skills/basic/gulp.svg",
   },
   {
     skill: "Next",
-    logo: "assets/svg/skills/next.svg",
+    logo: "assets/svg/skills/basic/next.svg",
+  },
+  {
+    skill: "GraphQL",
+    logo: "assets/svg/skills/basic/graphql.svg",
   },
   {
     skill: "Node",
-    logo: "assets/svg/skills/node.svg",
+    logo: "assets/svg/skills/basic/node.svg",
+  },
+  {
+    skill: "Express",
+    logo: "assets/svg/skills/basic/express.svg",
+  },
+  {
+    skill: "MongoDb",
+    logo: "assets/svg/skills/basic/mongodb.svg",
+  },
+  {
+    skill: "MySQL",
+    logo: "assets/svg/skills/basic/mysql.svg",
   },
   {
     skill: "Redux Saga",
-    logo: "assets/svg/skills/redux-saga.svg",
+    logo: "assets/svg/skills/basic/redux-saga.svg",
+  },
+  {
+    skill: "Jest",
+    logo: "assets/svg/skills/basic/jest.svg",
+  },
+  {
+    skill: "Testing Library",
+    logo: "assets/svg/skills/basic/testing-library.svg",
   },
   {
     skill: "Chart.js",
-    logo: "assets/svg/skills/chart.svg",
+    logo: "assets/svg/skills/basic/chart.svg",
+  },
+  {
+    skill: "Figma",
+    logo: "assets/svg/skills/basic/figma.svg",
   },
   {
     skill: "jQuery",
-    logo: "assets/svg/skills/jquery.svg",
+    logo: "assets/svg/skills/basic/jquery.svg",
   },
   {
     skill: "Pug",
-    logo: "assets/svg/skills/pug.svg",
+    logo: "assets/svg/skills/basic/pug.svg",
   },
   {
     skill: "Python",
-    logo: "assets/svg/skills/python.svg",
+    logo: "assets/svg/skills/basic/python.svg",
   },
   {
     skill: "Java",
-    logo: "assets/svg/skills/java.svg",
+    logo: "assets/svg/skills/basic/java.svg",
   },
   {
     skill: "C++",
-    logo: "assets/svg/skills/c++.svg",
+    logo: "assets/svg/skills/basic/c++.svg",
   },
 ];
 export { Proficient, Familiar };
