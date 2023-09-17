fc7ce4b420ff8b30091cbdff7f187e8b2843c49d
Andrey <antonov.andrey.10101@gmail.com>
2023-09-17 18:22:41 +0300

    feat: add NewAbout

---
+++ src/components/NewAbout/NewAbout.tsx
@@ -0,0 +1,22 @@
+// import './Sidebar.scss';
+
+function NewAbout() {
+  return (
+    <>
+      <header>
+        <h2 className="h2 article-title">About me</h2>
+      </header>
+
+      <section className="about-text">
+        <p>fff
+        </p>
+
+        <p>
+fff
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default NewAbout;
