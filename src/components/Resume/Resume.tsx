dc6dc223a8333892692682e9307b9756ce952c54
Andrey <antonov.andrey.10101@gmail.com>
2023-09-18 12:02:02 +0300

    feat: add icons to Resume section

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,4 +1,5 @@
 // import './Resume.scss';
+import { IoBriefcaseOutline, IoBookOutline } from 'react-icons/io5';
 import { v4 as uuidv4 } from 'uuid';
 import workHistory from '../../data/experienceData';
 
@@ -9,31 +10,11 @@ function Resume() {
         <h2 className="h2 article-title">Опыт работы</h2>
       </header>
 
-      {/* <section className="timeline">
-        <div className="title-wrapper">
-          <div className="icon-box"> <ion-icon name="book-outline"></ion-icon> </div>
-
-          <h3 className="h3">Education</h3>
-        </div>
-
-        <ol className="timeline-list">
-          <li className="timeline-item">
-            <h4 className="h4 timeline-item-title">University school of the arts</h4>
-
-            <span>2007 — 2008</span>
-
-            <p className="timeline-text">
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et quas
-              molestias exceptur.
-            </p>
-          </li>
-
-        </ol>
-      </section> */}
-
       <section className="timeline">
         <div className="title-wrapper">
-          <div className="icon-box">{/* <ion-icon name="book-outline"></ion-icon> */}</div>
+          <div className="icon-box">
+            <IoBriefcaseOutline />
+          </div>
           <h3 className="h3">Опыт</h3>
         </div>
 
@@ -50,61 +31,30 @@ function Resume() {
                   </h4>
                 ))}
               </div>
-              {/* <p className="timeline-text">{work.workDes}</p> */}
             </li>
           ))}
         </ol>
       </section>
 
-      {/* <section className="skill">
-        <h3 className="h3 skills-title">My skills</h3>
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
+            <IoBookOutline />
+          </div>
 
-        <ul className="skills-list content-card">
-          <li className="skills-item">
-            <div className="title-wrapper">
-              <h5 className="h5">Web design</h5>
-              <data value="80">80%</data>
-            </div>
+          <h3 className="h3">Education</h3>
+        </div>
 
-            <div className="skill-progress-bg">
-              <div className="skill-progress-fill" style={{ width: '80%' }} />
-            </div>
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Санкт-Петербургский политехнический университет Петра Великого</h4>
+
+            <span>2020 — 2024</span>
+
+            <p className="timeline-text">Институт компьютерных наук и технологий, Системный анализ и управление</p>
           </li>
-
-          <li className="skills-item">
-            <div className="title-wrapper">
-              <h5 className="h5">Graphic design</h5>
-              <data value="70">70%</data>
-            </div>
-
-            <div className="skill-progress-bg">
-              <div className="skill-progress-fill" style={{ width: '70%' }} />
-            </div>
-          </li>
-
-          <li className="skills-item">
-            <div className="title-wrapper">
-              <h5 className="h5">Branding</h5>
-              <data value="90">90%</data>
-            </div>
-
-            <div className="skill-progress-bg">
-              <div className="skill-progress-fill" style={{ width: '90%' }} />
-            </div>
-          </li>
-
-          <li className="skills-item">
-            <div className="title-wrapper">
-              <h5 className="h5">WordPress</h5>
-              <data value="50">50%</data>
-            </div>
-
-            <div className="skill-progress-bg">
-              <div className="skill-progress-fill" style={{ width: '50%' }} />
-            </div>
-          </li>
-        </ul>
-      </section> */}
+        </ol>
+      </section>
     </article>
   );
 }
