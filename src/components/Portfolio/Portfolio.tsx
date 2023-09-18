aa47ebda374b31be36d5fdce8d049a00261580de
Andrey <antonov.andrey.10101@gmail.com>
2023-09-18 15:22:37 +0300

    feat: add select for portfolio on mobile devices

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable react/no-array-index-key */
 import { useEffect, useState } from 'react';
 import { BsLink45Deg, BsGithub } from 'react-icons/bs';
+import filters from '../../data/filtersData';
 import projects from '../../data/projectData';
 import './Portfolio.scss';
 
@@ -24,90 +24,40 @@ function Portfolio() {
 
       <section className="projects">
         <ul className="filter-list">
-          <li className="filter-item">
-            <button type="button" className={filter === 'all' ? 'active' : ''} onClick={() => setFilter('all')}>
-              Все
-            </button>
-          </li>
-
-          <li className="filter-item">
-            <button type="button" className={filter === 'react' ? 'active' : ''} onClick={() => setFilter('react')}>
-              React
-            </button>
-          </li>
-
-          {/* <li className="filter-item">
-            <button type="button" className={filter === 'vue' ? 'active' : ''} onClick={() => setFilter('vue')}>
-              Vue
-            </button>
-          </li> */}
-
-          <li className="filter-item">
-            <button type="button" className={filter === 'test' ? 'active' : ''} onClick={() => setFilter('test')}>
-              Тестовые задания
-            </button>
-          </li>
-
-          {/* <li className="filter-item">
-            <button type="button" className={filter === 'fm' ? 'active' : ''} onClick={() => setFilter('fm')}>
-              Frontend Mentor
-            </button>
-          </li> */}
-
-          <li className="filter-item">
-            <button type="button" className={filter === 'js' ? 'active' : ''} onClick={() => setFilter('js')}>
-              Native JS
-            </button>
-          </li>
+          {filters.map((filterItem) => (
+            <li className="filter-item" key={filterItem.value}>
+              <button
+                type="button"
+                className={filter === filterItem.value ? 'active' : ''}
+                onClick={() => setFilter(filterItem.value)}
+              >
+                {filterItem.label}
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="filter-select-box">
-          <button type="button" className="filter-select" data-select>
-            <div className="select-value" data-selecct-value>
-              Select category
-            </div>
-
-            <div className="select-icon">{/* <ion-icon name="chevron-down"></ion-icon> */}</div>
-          </button>
-
-          <ul className="select-list">
-            <li className="select-item">
-              <button type="button" data-select-item>
-                All
-              </button>
-            </li>
-
-            <li className="select-item">
-              <button type="button" data-select-item>
-                Web design
-              </button>
-            </li>
-
-            <li className="select-item">
-              <button type="button" data-select-item>
-                Applications
-              </button>
-            </li>
-
-            <li className="select-item">
-              <button type="button" data-select-item>
-                Web development
-              </button>
-            </li>
-          </ul>
+          <select name="select" className="filter-select" onChange={(e) => setFilter(e.target.value)}>
+            {filters.map((filterItem) => (
+              <option className="select-item" key={filterItem.value} value={filterItem.value}>
+                {filterItem.label}
+              </option>
+            ))}
+          </select>
         </div>
 
         <ul className="project-list">
-          {filterProjects.map((project, index) => (
-            <li className="project-item" key={index}>
+          {filterProjects.map((project) => (
+            <li className="project-item" key={project.title}>
               <figure className="project-img">
                 <img src={project.image} alt={project.title} />
               </figure>
               <div className="project__info">
                 <h3 className="project-title">{project.title}</h3>
                 <div className="project-tools">
-                  {project.tools.map((disc, toolIndex) => (
-                    <span className="project__tool" key={toolIndex}>
+                  {project.tools.map((disc) => (
+                    <span className="project__tool" key={disc}>
                       {disc}
                     </span>
                   ))}
---
+++ src/data/filtersData.ts
@@ -0,0 +1,18 @@
+const filtersData = [
+    {
+        value: 'all',
+        label: 'Все'
+    },
+    {
+        value: 'react',
+        label: 'React'
+    }, {
+        value: 'test',
+        label: 'Тестовые задания'
+    }, {
+        value: 'js',
+        label: 'Native'
+    }
+]
+
+export default filtersData;
